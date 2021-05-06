d3770bcf8695c4827d2f91a030c2588039ec278c
jean <jin.ahn.1027@gmail.com>
2021-05-06 18:26:18 -0400

    added functionality of edit and delete of favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,7 +8,7 @@ class FavoritesController < ApplicationController
     def create
         @favorite = current_user.favorites.build(favorite_params)
         if @favorite.save
-            redirect_to user_favorite_path(current_user, @favorite)
+            redirect_to favorite_path(@favorite)
         else
             render :new
         end    
@@ -17,14 +17,34 @@ class FavoritesController < ApplicationController
     def index
         if current_user.favorites.empty?
             flash[:message] = "You don't have any list yet why don't you make one"
-            redirect_to new_user_favorite_path(current_user)
+            redirect_to new_favorite_path
         else
             @favorites=current_user.favorites
         end
     end
 
+    def edit
+
+    end
+
+    def update
+        @favorite.update(favorite_params)
+
+        if @favorite.valid?
+            redirect_to favorite_path(@favorite)
+        else
+            flash[:message] = "Unable to update"
+            render :edit
+        end
+    end
+
     def show
         
+    end
+
+    def destroy
+        @favorite.destroy
+        redirect_to favorites_path
     end
 
     private
